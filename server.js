a5be6716171251b49cea71122030616f023f7f5f
Junny <tajo0215@gmail.com>
2021-07-18 11:18:32 -0700

    server.js added, routes directory added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: htpp://localhost:${PORT}`);
+})
