9b01d1ef5ecc9c31f7a1078610ebeab678b8c638
Junny <tajo0215@gmail.com>
2021-07-18 11:41:14 -0700

    app.get for /api/notes/ routes implemented

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const noteTaking = require('../db');
+
+module.exports = app => {
+
+  app.get('/api/notes', (req, res) => {
+    noteTaking
+      .getAll()
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(500).end();
+      });
+  })
+
+  app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    
+  })
+}
